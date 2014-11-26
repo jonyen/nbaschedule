b9bf9daaf5f84e55af5281f1cdc90aa38eac5929
Jonathan Yen <jonyen@gmail.com>
2014-11-26 15:20:22 -0800

    updated parser to show remaining games

--- parse_schedule.py
+++ parse_schedule.py
@@ -53,31 +53,41 @@ magichome = 0
 knickshome = 0
 
 
+myguaranteed = 0
+johnnyguaranteed = 0
+
 games = 0
 x = json.load(f)
 for game in x['games']:
 #  if pytz.utc.localize(parse('2014-12-1')) < parse(game['scheduled']) and parse(game['scheduled']) < pytz.utc.localize(parse('2015-1-1')):
 #  if pytz.utc.localize(parse('2014-10-1')) < parse(game['scheduled']) and parse(game['scheduled']) < pytz.utc.localize(parse('2014-12-1')):
-  if pytz.utc.localize(parse('2014-11-18')) < parse(game['scheduled']) and parse(game['scheduled']) < pytz.utc.localize(parse('2014-12-1')):
+  if pytz.utc.localize(parse('2014-11-26')) < parse(game['scheduled']) and parse(game['scheduled']) < pytz.utc.localize(parse('2014-12-1')):
   #if pytz.utc.localize(parse('2015-1-1')) < parse(game['scheduled']) and parse(game['scheduled']) < pytz.utc.localize(parse('2015-2-1')):
   #if pytz.utc.localize(parse('2015-2-1')) < parse(game['scheduled']) and parse(game['scheduled']) < pytz.utc.localize(parse('2015-3-1')):
   #if pytz.utc.localize(parse('2015-3-1')) < parse(game['scheduled']) and parse(game['scheduled']) < pytz.utc.localize(parse('2015-4-1')):
     games += 1
     home = game['home']['alias']
     away = game['away']['alias']
-#    print game['scheduled']
+    stat = ""
+    if home in myteams and away in myteams:
+      stat = "+"
+      myguaranteed += 1
+    if home in johnnys and away in johnnys:
+      stat = "-" 
+      johnnyguaranteed += 1
+    print game['scheduled'] + " " + home + " " + away + " " + stat
     if home == 'MIL':
       buckshome += 1
-      print home + " " + away
+#      print home + " " + away
     if home == 'DET':
       pistonshome += 1
-      print home + " " + away
+#      print home + " " + away
     if home == 'ORL':
       magichome += 1
-      print home + " " + away
+#      print home + " " + away
     if home == 'NYK':
       knickshome += 1
-      print home + " " + away
+#      print home + " " + away
     if home in myteams:
       myhome += 1
     else:
@@ -88,25 +98,27 @@ for game in x['games']:
 
 total = 0
 for team in myteams:
-  print team + " " + str(teams[team])
+#  print team + " " + str(teams[team])
   total += teams[team]
-print total / 2
+#print total / 2
 print "-------" 
 total = 0
 for team in johnnys:
-  print team + " " + str(teams[team])
+#  print team + " " + str(teams[team])
   total += teams[team]
-print total / 2
+#print total / 2
+
+print "net guaranteed wins: " + str(myguaranteed) + " - " + str(johnnyguaranteed) + " = " + str(myguaranteed-johnnyguaranteed)
  
 #  print datetime(2014,12,1) < datetime(game['scheduled'])
 #  print datetime(game['scheduled']) < datetime(2015,1,1)
 #  print json.dumps(game, sort_keys=True, indent=4, separators=(',', ': '))
 #print json.dumps(x, sort_keys=True, indent=4, separators=(',', ': '))
-print str(myhome) + " : " + str(johnnyhome) + " / " + str(games)
+print "home games: " + str(myhome) + " + " + str(johnnyhome) + " = " + str(games)
 
-print "bucks home: " + str(buckshome)
-print "pistons home: " + str(pistonshome)
-print "magic home: " + str(magichome)
-print "knicks home: " + str(knickshome)
+#print "bucks home: " + str(buckshome)
+#print "pistons home: " + str(pistonshome)
+#print "magic home: " + str(magichome)
+#print "knicks home: " + str(knickshome)
 
 f.close()
