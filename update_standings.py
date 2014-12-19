91eade1002298d8b5d0803660a72c6dfca4cae89
jonyen <jonyen@gmail.com>
2014-12-18 16:29:06 -0800

    Adding December tally

--- update_standings.py
+++ update_standings.py
@@ -19,23 +19,31 @@ client.ClientLogin('jonyen@gmail.com', 'mllrojbpmkugsaoe')
 doc_key = '1tC2LR7slerVyxVLAmHYxYhvto3yLjim6xWTL2vLlTX8'
 
 mine_west = ['Spurs', 'Warriors', 'Rockets', 'Timberwolves', 'Lakers', 'Nuggets', 'Kings', 'Jazz']
-mine_east = ['Bulls', 'Pacers', 'Nets', 'Celtics', 'Pistons', 'Raptors', 'Bucks', '76ers']
+mine_east = ['Bulls', 'Pacers', 'Nets', 'Celtics', 'Magic', 'Raptors', 'Knicks', '76ers']
 johnny_west = ['Clippers', 'Thunder', 'Mavericks', 'Grizzlies', 'Trail Blazers', 'Suns', 'Pelicans', 'Jazz']
-johnny_east = ['Cavaliers', 'Wizards', 'Heat', 'Hornets', 'Hawks', 'Knicks', 'Magic', '76ers']
+johnny_east = ['Cavaliers', 'Wizards', 'Heat', 'Hornets', 'Hawks', 'Bucks', 'Pistons', '76ers']
 
 for conference in standings['conferences']:
   for division in conference['divisions']:
     for team in division['teams']:
       print team['name'] + " " + str(team['wins'])
       if team['name'] in mine_west:
-        client.UpdateCell(4 + mine_west.index(team['name']), 5, str(team['wins']), doc_key, 5)
+        prev = client.GetCellsFeed(doc_key, 5, 'R' + str(4 + mine_west.index(team['name'])) + 'C6').cell.text
+	print "prev: " + team['name'] + ' ' + prev
+        client.UpdateCell(4 + mine_west.index(team['name']), 7, str(team['wins'] - int(prev)), doc_key, 5)
       if team['name'] in mine_east:
-        client.UpdateCell(13 + mine_east.index(team['name']), 5, str(team['wins']), doc_key, 5)
+        prev = client.GetCellsFeed(doc_key, 5, 'R' + str(13 + mine_east.index(team['name'])) + 'C6').cell.text
+	print "prev: " + team['name'] + ' ' + prev
+        client.UpdateCell(13 + mine_east.index(team['name']), 7, str(team['wins'] - int(prev)), doc_key, 5)
       if team['name'] in johnny_west:
-        client.UpdateCell(4 + johnny_west.index(team['name']), 3, str(team['wins']), doc_key, 5)
+        prev = client.GetCellsFeed(doc_key, 5, 'R' + str(4 + johnny_west.index(team['name'])) + 'C3').cell.text
+	print "prev: " + team['name'] + ' ' + prev
+        client.UpdateCell(4 + johnny_west.index(team['name']), 4, str(team['wins'] - int(prev)), doc_key, 5)
       if team['name'] in johnny_east:
-        client.UpdateCell(13 + johnny_east.index(team['name']), 3, str(team['wins']), doc_key, 5)
+        prev = client.GetCellsFeed(doc_key, 5, 'R' + str(13 + johnny_east.index(team['name'])) + 'C3').cell.text
+	print "prev: " + team['name'] + ' ' + prev
+        client.UpdateCell(13 + johnny_east.index(team['name']), 4, str(team['wins'] - int(prev)), doc_key, 5)
 
-client.UpdateCell(3, 7, datetime.datetime.now().strftime("Last updated: %c"), doc_key, 5)
+client.UpdateCell(3, 9, datetime.datetime.now().strftime("Last updated: %c"), doc_key, 5)
 
 #print client.GetCellsFeed(doc_key, 5, "R1C1")
